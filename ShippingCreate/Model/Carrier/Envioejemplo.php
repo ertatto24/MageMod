e36ed69cf9867e634b908c4c6200d1ca7ccc50c1
Jose Manuel <ertatto@gmail.com>
2023-09-16 17:33:14 +0200

    Add ShippingCreate module

---
+++ ShippingCreate/Model/Carrier/Envioejemplo.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Copyright © Devsarrollo All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Devsarrollo\ShippingCreate\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Rate\Result;
+
+class Envioejemplo extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
+    \Magento\Shipping\Model\Carrier\CarrierInterface
+{
+
+    protected $_code = 'envioejemplo';
+
+    protected $_isFixed = true;
+
+    protected $_rateResultFactory;
+
+    protected $_rateMethodFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
+        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        $this->_rateResultFactory = $rateResultFactory;
+        $this->_rateMethodFactory = $rateMethodFactory;
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $shippingPrice = $this->getConfigData('price');
+
+        $result = $this->_rateResultFactory->create();
+
+        if ($shippingPrice !== false) {
+            $method = $this->_rateMethodFactory->create();
+
+            $method->setCarrier($this->_code);
+            $method->setCarrierTitle($this->getConfigData('title'));
+
+            $method->setMethod($this->_code);
+            $method->setMethodTitle($this->getConfigData('name'));
+
+            if ($request->getFreeShipping() === true) {
+                $shippingPrice = '0.00';
+            }
+
+            $method->setPrice($shippingPrice);
+            $method->setCost($shippingPrice);
+
+            $result->append($method);
+        }
+
+        return $result;
+    }
+
+    /**
+     * getAllowedMethods
+     *
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+}
