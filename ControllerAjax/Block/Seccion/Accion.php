066899f557dd9707a0042303da1ef47700d96368
Jose Manuel <ertatto@gmail.com>
2023-09-16 02:14:55 +0200

    Add ControllerAjax module

---
+++ ControllerAjax/Block/Seccion/Accion.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © Devsarrollo All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Devsarrollo\ControllerAjax\Block\Seccion;
+
+class Accion extends \Magento\Framework\View\Element\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
---
+++ ControllerAjax/README.md
@@ -0,0 +1,7 @@
+# MageMod
+## Módulos genéricos para magento
+Listado de módulos básicos para tener una base de la que partir y desarrollar o solucionar lógicas más complejas.
+
+### ControllerAjax
+Creación de un controlador básico llamado por vía Ajax.
+- URL/pagina/seccion/accion
